25748c294d8eedbb1a84cdee121ce94c31430c41
Arya <aryapratimray@gmail.com>
2022-11-29 14:50:41 +0530

    added database

--- app.js
+++ app.js
@@ -1,33 +1,81 @@
 const express = require("express")
-const bodyParser =  require("body-parser")
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
 
 const app = express()
-var items = ["Buy Food" , "Cook Food" , "Eat Food"]
 
 app.set('view engine', 'ejs');   //templating for repeated html codes
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"))
 
-app.get("/",function(req,res){
-    var today = new Date();
-    var options = {
-        weekday : "long",
-        day : "numeric",
-        month : "long",
-    };
+mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true })
 
-    var day = today.toLocaleDateString("en-US",options);  //render todays date and use the options
+const itemsSchema = {
+    name: String
+};
 
-    res.render('list', {theDay: day , newListItem: items});  //search in file list.ejs the variable theDay and send day in it
+const Item = mongoose.model("Item", itemsSchema)
+
+const item1 = new Item({
+    name: "Welcome to To-Do-List"
 });
 
-app.post("/",function(req,res){
-    var item = req.body.newItem  //post the html input tag with name newItem
-    items.push(item)
-    res.redirect("/");           //send new task on todo list to  html 
+const item2 = new Item({
+    name: "Click on + to add new item"
 });
 
-app.listen(3000,function(){
+const item3 = new Item({
+    name: "Hit this to delete item"
+});
+
+const defaultItems = [item1, item2, item3]
+
+app.get("/", function (req, res) {
+    Item.find({}, function (err, foundItems) {
+
+        if (foundItems.length === 0) {
+            Item.insertMany(defaultItems, function (err) {
+                if (err) {
+                    console.log(err)
+                }
+                else {
+                    console.log("Successful")
+                }
+            });
+            res.redirect("/")
+        }
+        else{
+            res.render("list", { listTitle: "Today", newListItem: foundItems });  //search in file list.ejs the variable theDay and send day in it
+        }
+    });
+});
+
+app.post("/", function (req, res) {
+    const itemName = req.body.newItem  //post the html input tag with name newItem
+
+    const item = new Item({
+        name: itemName
+    });
+    item.save()
+    res.redirect("/")
+});
+
+app.post("/delete", function (req, res) {
+    const checkedItemId = req.body.checkbox
+
+    Item.findByIdAndRemove(checkedItemId, function(err){
+        if(!err){
+            console.log("Successfully deleted")
+            res.redirect("/")
+        }
+    })
+});
+
+app.get("/work", function(req,res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems})
+});
+
+app.listen(3000, function () {
     console.log("server running on port 3000")
 });
 
